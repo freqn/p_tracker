dacc6cdeea0c6ab107e81527348e440eba79b2d4
c <freqnlabs@gmail.com>
2014-04-20 09:37:05 -0400

    Generated basic scaffolding, added validations & configured index route

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThingsIBought::Application.config.secret_key_base = '0f9d3b4bf139ca50324c702185e5bd6336bbee45b9c31043245de1c7f22e144898d89fac10a6e71d8b719e62faed36b53c045caa1a5b22953d1919319873b2b0'
